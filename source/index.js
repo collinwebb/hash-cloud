3b59edfea604b0562efcbd886b17e62ff9b3dbbb
Collin Webb <collindwebb@gmail.com>
2015-07-09 14:26:47 -0700

    creates usable follow button

--- source/index.js
+++ source/index.js
@@ -9,7 +9,6 @@ app.controller("mainCtrl", function($scope, twitterUser) {
   $scope.search = function() {
     twitterUser.search($scope.words)
     .success(function(data) {
-      console.log(data);
       $scope.data = data;
     })
     .catch(function(error) {
@@ -23,10 +22,23 @@ app.controller("mainCtrl", function($scope, twitterUser) {
     twitterUser.sendTweet($scope.tweet)
     .success(function(resp) {
       $scope.tweet = "";
+    })
+    .catch(function(error) {
+      console.log(error);
     });
   };
 
   $scope.includeInTweet = function(tag) {
     $scope.tweet = $scope.tweet + " " + tag;
+  };
+
+  $scope.follow = function (screenName){
+    twitterUser.follow(screenName)
+    .success(function(data) {
+      $scope.data.people[screenName].following = true;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
   };
 });
--- source/models/twitterUser.js
+++ source/models/twitterUser.js
@@ -20,6 +20,11 @@ angular.module('sif')
     }
   };
 
+  this.follow = function(screenName){
+    var data = withTokens({screen_name: screenName});
+    return $http.post(urls.apiUrl + "/follow", data);
+  };
+
   this.search = function(words) {
     var data = withTokens({ words: words });
     return $http.post(urls.apiUrl + "/search", data);
